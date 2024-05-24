a759b923f1b352d45a4e99d96bb5fb607bf5f061
rjaychen <aang0616@gmail.com>
2024-05-23 20:15:54 -0400

    fastsam + mobilenet-ssd opencv inpainting

--- Detector.py
+++ Detector.py
@@ -22,7 +22,7 @@ class Detector:
         with open(self.classesPath, 'r') as f:
             self.classesList = f.read().splitlines()
             self.colorList = np.random.uniform(low=0, high=255, size=(len(self.classesList), 3))
-        # self.classesList.insert(0, '__Background__') # add back for certain models 
+        self.classesList.insert(0, '__Background__') # remove for yolov4, add for mobilenet-ssd
 
     def getBbox(self, frame):
         classLabelIDs, confidences, bboxs = self.net.detect(frame, confThreshold=0.6)
@@ -67,11 +67,15 @@ class Detector:
         #     x,y,w,h = 0, 0, 1, 1
         #     pass
 
+        (x, y, w, h) , classColor, displayText = self.getBbox(frame)
+
         cv2.rectangle(frame, (x,y), (x+w, y+h), color=(255,0,0), thickness=1)
         cv2.putText(frame, displayText, (x, y-10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
 
+        
         mask = np.zeros(frame.shape[:2], dtype="uint8")
         cv2.rectangle(mask, (x,y), (x+w, y+h), 255, -1)
+        # print(mask.shape)
         if useBlurInstead:
             inpainted = frame 
             cropped = frame[y:y+h, x:x+w]
